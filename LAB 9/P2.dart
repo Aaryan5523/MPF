4a0d1ff4db30c576308838ea940ddda984b78493
Aaryan5523 <aaryanvidja31@gmail.com>
2025-07-15 14:28:09 +0530

    Add files via upload

---
+++ LAB 9/P2.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+class qvoteapp extends StatefulWidget {
+  const qvoteapp({super.key});
+
+  @override
+  State<qvoteapp> createState() => _qvoteappState();
+}
+
+class _qvoteappState extends State<qvoteapp> {
+  Widget qvotecard({required String text,required String author}){
+    return Card(
+      child: ListTile(
+        title: Text(text,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+        subtitle: Text(author,style: TextStyle(fontSize: 18,color: Colors.grey),),
+        trailing: IconButton(onPressed: (){
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text("copied to clipboard")));
+        },
+            icon: Icon(Icons.copy,color: Colors.red,)),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Feedback Form"),
+          backgroundColor: Colors.grey,
+        ),
+        body: Column(
+            children: [
+              qvotecard(text: "how wouid you desc your mood after using our product for first time",author: "denish"),
+              qvotecard(text: "How wouid you rate this product",author: "denish"),
+              qvotecard(text: "Your feedback",author: "denish"),
+            ]
+        ),
+      ),
+    );
+  }
+}
